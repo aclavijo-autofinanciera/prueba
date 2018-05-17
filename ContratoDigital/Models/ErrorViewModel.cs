fa3755ce5c4d44999660c461265d178f07eec309
Luis Hernando Robles Bello <luhero04@autofinanciera.local>
2018-05-17 11:09:14 -0500

    Add project files.

---
+++ ContratoDigital/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContratoDigital.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
