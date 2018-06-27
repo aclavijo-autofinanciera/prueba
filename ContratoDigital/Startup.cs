f76b463a96b1aaa724ce5f0c86f4b1894a718cc3
Luis Robles <dracvs@gmail.com>
2018-06-27 18:29:27 -0500

    Update a ASP.NET COre 2.1.1

--- ContratoDigital/Startup.cs
+++ ContratoDigital/Startup.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContratoDigital.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -21,7 +25,17 @@ namespace ContratoDigital
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+            services.AddDbContext<ContratoDigitalContext>(options => options
+            .UseLazyLoadingProxies()
+            .UseSqlServer("Data Source=localhost;Initial Catalog=ContratoDigital;Integrated Security=True"));
+            //services.AddDbContext<ContratoDigitalContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddDbContext<ContratoDigitalContext>(options => options.UseSqlServer("Data Source=localhost;Initial Catalog=ContratoDigital;User Id=SA;Password=NissanGTR2018$;"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,15 +43,18 @@ namespace ContratoDigital
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
+                //app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
