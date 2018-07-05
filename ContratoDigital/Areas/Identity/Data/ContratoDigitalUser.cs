0bb0af38241edd96294b1a90ee41aa4b6ae4f9fa
Luis Robles <dracvs@gmail.com>
2018-07-05 10:31:25 -0500

    Vistas de manejo de usuarios

---
+++ ContratoDigital/Areas/Identity/Data/ContratoDigitalUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace ContratoDigital.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the ContratoDigitalUser class
+    public class ContratoDigitalUser : IdentityUser
+    {
+        [PersonalData]
+        public string Nombre { get; set; }
+
+        [PersonalData]
+        public string Apellido { get; set; }
+
+        [PersonalData]
+        public int Agencia { get; set; }
+
+        [PersonalData]
+        public string IdSiicon { get; set; }
+
+        [PersonalData]
+        public int Asesor { get; set; }
+
+    }
+}
---
+++ ContratoDigital/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ContratoDigital.Areas.Identity.Data;
+using ContratoDigital.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ContratoDigital.Areas.Identity.IdentityHostingStartup))]
+namespace ContratoDigital.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UsuariosContext>(options =>
+                options.UseSqlServer(context.Configuration.GetConnectionString("UsuariosContextConnection")));
+                    //options.UseSqlServer("Data Source=localhost;Initial Catalog=Usuarios;Integrated Security=True"));
+                        //context.Configuration.GetConnectionString("UsuariosContextConnection")));
+
+                services.AddDefaultIdentity<ContratoDigitalUser>(options=> {
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+                    options.SignIn.RequireConfirmedEmail = false;
+                    options.SignIn.RequireConfirmedPhoneNumber = false;
+                })
+                    .AddRoles<IdentityRole>()
+                    .AddRoleManager<RoleManager<IdentityRole>>()
+                    .AddEntityFrameworkStores<UsuariosContext>();
+            });
+        }
+    }
+}
