b56d1a61697cca2913fbea43af424c8961c6e881
Luis Robles <dracvs@gmail.com>
2018-07-09 09:48:29 -0500

    Archivos de conexión de Schema y base de datos

---
+++ ContratoDigital/Migrations/20180703165757_CustomUserData.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContratoDigital.Migrations
+{
+    public partial class CustomUserData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Agencia",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Apellido",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IdSiicon",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nombre",
+                table: "AspNetUsers",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Asesor",
+                table: "AspNetUsers",
+                nullable: false,
+                defaultValue:0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Agencia",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Apellido",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "IdSiicon",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Nombre",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Asesor",
+                table: "AspNetUsers");
+        }
+    }
+}
