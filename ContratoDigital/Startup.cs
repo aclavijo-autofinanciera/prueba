e22ed41bb8cff57702924e90d422b5118bbb02fa
Luis Robles <dracvs@gmail.com>
2018-07-09 09:44:02 -0500

    Configuración de servicio de envío de correos

--- ContratoDigital/Startup.cs
+++ ContratoDigital/Startup.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContratoDigital.Areas.Identity.Data;
 using ContratoDigital.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -32,14 +34,26 @@ namespace ContratoDigital
             });
             services.AddDbContext<ContratoDigitalContext>(options => options
             .UseLazyLoadingProxies()
-            .UseSqlServer("Data Source=localhost;Initial Catalog=ContratoDigital;Integrated Security=True"));
+            .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //.UseSqlServer("Data Source=localhost;Initial Catalog=ContratoDigital;Integrated Security=True"));
             //services.AddDbContext<ContratoDigitalContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             //services.AddDbContext<ContratoDigitalContext>(options => options.UseSqlServer("Data Source=localhost;Initial Catalog=ContratoDigital;User Id=SA;Password=NissanGTR2018$;"));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            
+            services.AddMemoryCache();
+            services.AddSingleton<IEmailConfiguration>(Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>());
+            services.AddTransient<IEmailService, EmailService>();
+            services.AddSession();
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddSessionStateTempDataProvider();
+
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -54,7 +68,10 @@ namespace ContratoDigital
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
+            app.UseSession();
+
 
             app.UseMvc(routes =>
             {
@@ -62,6 +79,9 @@ namespace ContratoDigital
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
+
+       
     }
 }
