0aa72734e2b4ceab6800dc0f355b3457230bbc3c
Luis Robles <dracvs@gmail.com>
2018-07-10 15:30:25 -0500

    Creación de clase de confirmación de contrato

---
+++ ContratoDigital/Models/ConfirmacionContrato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContratoDigital.Models
+{
+    public class ConfirmacionContrato
+    {
+        [Key]
+        public int Id { get; set; }
+        public int IdContrato { get; set; }
+        public string Guuid { get; set; }
+        public bool IsIdUploaded { get; set; }
+        public bool IsAccepted { get; set; }
+        public bool IsPaid { get; set; }
+        public bool IsVerified { get; set; }
+        public DateTime FechaVerificacion { get; set; }
+        public DateTime FechaAceptacion { get; set; }
+        public DateTime FechaPago { get; set; }
+
+        public virtual Contrato Contrato { get; set; }
+    }
+}
