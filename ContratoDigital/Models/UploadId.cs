aeda1146c3d194148165362bda5c7ede7b31a35c
Luis Robles <dracvs@gmail.com>
2018-08-06 10:45:15 -0500

    Clase modelo para el Upload del documento de identificación

---
+++ ContratoDigital/Models/UploadId.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ContratoDigital.Models
+{
+    public class UploadId
+    {
+
+        [Key]
+        public int IdUpload { get; set; }
+        public int IdContrato { get; set; }
+        [NotMapped]
+        public IFormFile Anverso { get; set; }
+        [NotMapped]
+        public IFormFile Reverso { get; set; }
+    }
+}
