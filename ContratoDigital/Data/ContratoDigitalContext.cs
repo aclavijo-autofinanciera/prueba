af0fecc0ce88ab58cc6310e4ca53ec730fd83651
Luis Robles <dracvs@gmail.com>
2018-08-31 11:19:25 -0500

    Actualización de conexión a base de datos.

--- ContratoDigital/Data/ContratoDigitalContext.cs
+++ ContratoDigital/Data/ContratoDigitalContext.cs
@@ -15,11 +15,15 @@ namespace ContratoDigital.Data
     {
         public ContratoDigitalContext(DbContextOptions<ContratoDigitalContext> options) : base(options){}
 
-        public DbSet<Contrato> Contratos { get; set; }
-        public DbSet<Prospecto> Prospectos { get; set; }
+        public DbSet<Ciudades> Ciudades { get; set; }
         public DbSet<ConfirmacionContrato> ConfirmacionContratos { get; set; }
         public DbSet<ConfirmacionProspecto> ConfirmacionProspectos { get; set; }
-        public DbSet<UploadId> UploadId { get; set; }
+        public DbSet<Contrato> Contratos { get; set; }
         public DbSet<DocumentoIdentidad> DocumentoIdentidad { get; set; }
+        public DbSet<Estado> Estados { get; set; }
+        public DbSet<Prospecto> Prospectos { get; set; }        
+        public DbSet<TipoEstado> TipoEstados { get; set; }
+        public DbSet<UploadId> UploadId { get; set; }
+        
     }
 }
