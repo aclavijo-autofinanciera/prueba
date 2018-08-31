d1d44c5fc55772f49d07a5d35c1cb6dafa112941
Luis Robles <dracvs@gmail.com>
2018-08-31 11:20:35 -0500

    Controlador de Dashboard. Creación de usuarios.

---
+++ ContratoDigital/Controllers/DashboardController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContratoDigital.Areas.Identity.Data;
+using ContratoDigital.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ContratoDigital.Controllers
+{
+    [Authorize]    
+    public class DashboardController : Controller
+    {
+        private readonly ContratoDigitalContext _context;
+        private readonly UserManager<ContratoDigitalUser> _userManager;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IEmailConfiguration _emailConfiguration;
+        public DashboardController(IHostingEnvironment hostingEnvironment, ContratoDigitalContext context, IEmailConfiguration emailConfiguration, UserManager<ContratoDigitalUser> userManager)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _context = context;
+            _emailConfiguration = emailConfiguration;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region Users
+        public IActionResult Users()
+        {
+            return View();
+        }
+
+        public IActionResult AddUser()
+        {
+            return View(new ContratoDigitalUser());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(IFormCollection form)
+        {
+            var user = new ContratoDigitalUser();
+            user.UserName = form["UserName"];
+            user.Email = form["Email"];
+            user.EmailConfirmed = true;
+
+            user.Nombre = form["Nombre"];
+            user.Apellido = form["Apellido"];
+            int.TryParse(s: form["Agencia"], result:  out int agencia);            
+            user.Cedula = form["Cedula"];
+            string userPwd = form["Password"];
+            user.Agencia = agencia;
+
+            WebserviceController service = new WebserviceController(_context);
+            string resultSiicon = service.GetSiiconUserId(user.Cedula).Result.Value;            
+            string resultAsesor = service.GetAsesorId(user.Cedula).Result.Value;
+
+            string first = resultSiicon;
+            string second = resultAsesor;
+
+            if (!String.IsNullOrEmpty(resultSiicon) && resultSiicon != "[]")
+            {
+                dynamic jsonSiicon = JsonConvert.DeserializeObject<dynamic>(resultSiicon);
+                user.IdSiicon = jsonSiicon.First.TerceroId;
+            }
+            else
+            {
+                user.IdSiicon = ""; 
+            }
+            if(!String.IsNullOrEmpty(resultAsesor) && resultAsesor!="[]")
+            {
+                dynamic jsonAsesor = JsonConvert.DeserializeObject<dynamic>(resultAsesor);
+                user.Asesor = jsonAsesor.First.CodAsesor;
+            }
+            else
+            {
+                user.Asesor = 0;
+            }
+
+            if(user.Asesor.Equals(0) && String.IsNullOrEmpty(user.IdSiicon))
+            {
+                ViewData["Error"] = "El usuario no pudo ser creado por que no tiene un usuario o número de asesor asignado en el SIICON";
+                return View(user);                
+            }
+
+            IdentityResult chkUser = await _userManager.CreateAsync(user, userPwd);
+            if (chkUser.Succeeded)
+            {
+                var result = await _userManager.AddToRoleAsync(user, form["Rol"]);
+            }
+            return RedirectToAction("UserDetails", "Dashboard", new { id = user.UserName });
+        }
+
+        public IActionResult UserDetails(string id)
+        {
+            ContratoDigitalUser user = _userManager.FindByNameAsync(id).Result;
+            if(user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                ViewData["Error"] = "El usuario no existe";
+                return View(new ContratoDigitalUser());
+            }
+            
+        }
+        #endregion
+    }
+}
