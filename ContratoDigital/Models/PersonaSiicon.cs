c328161324d3d78c4fa7492554fdc9097410f571
Luis Robles <dracvs@gmail.com>
2018-09-17 11:47:52 -0500

    Clase de llenado para personas de SIICON

--- ContratoDigital/Models/PersonaSiicon.cs
+++ ContratoDigital/Models/PersonaSiicon.cs
@@ -12,7 +12,7 @@ namespace ContratoDigital.Models
         public int TipoIdentificacionRepreLegalId { get; set; } // EXCEL ANEXO
         public int TipoPersonaId { get; set; } //, EXCEL ANEXO        
         public string NumeroIdentificacionRepreLegal { get; set; } //, (TEXTO)
-        public int CiudadConstitucionId { get; set; } //, EXCEL ANEXO
+        public string CiudadConstitucionId { get; set; } //, EXCEL ANEXO
         public string FechaConstitucion { get; set; } //, (TEXTO = MM / DD / AAAA) Si aplica
         public string PrimerNombre { get; set; } //, (TEXTO) Solo persona natural
         public string SegundoNombre { get; set; } // , (TEXTO) Solo persona natural
@@ -22,9 +22,9 @@ namespace ContratoDigital.Models
         public int TipoDocumentoIdentidadId { get; set; } //, EXCEL ANEXO
         public string NumeroDocumento { get; set; } // , (TEXTO)
         public string DigitoVerificacion { get; set; } //, (TEXTO) Solo persona jurídica
-        public int CiudadExpedicionId { get; set; } //, EXCEL ANEXO
+        public string CiudadExpedicionId { get; set; } //, EXCEL ANEXO
         public string FechaNacimiento { get; set; } //, (TEXTO = MM / DD / AAAA) Si aplica
-        public int CiudadNacimientoId { get; set; } //, EXCEL ANEXO
+        public string CiudadNacimientoId { get; set; } //, EXCEL ANEXO
         public int SexoId { get; set; } //, EXCEL ANEXO
         public int EstadoCivilId { get; set; } //, EXCEL ANEXO
         public string Email { get; set; } //, (TEXTO)
@@ -33,7 +33,7 @@ namespace ContratoDigital.Models
         public string TelefonoNotifiacion { get; set; } //, (TEXTO)
         public string CelularNotificacion { get; set; } //, (TEXTO)
         public int DepartamentoNotificacionId { get; set; } //, EXCEL ANEXO
-        public int CiudadNotificacionId { get; set; } //, EXCEL ANEXO
+        public string CiudadNotificacionId { get; set; } //, EXCEL ANEXO
         public string EmpresaLabora { get; set; } //, (TEXTO)
         public string CargoLabora { get; set; } //, (TEXTO)
         public string DireccionLabora { get; set; } //, (TEXTO)
@@ -41,7 +41,7 @@ namespace ContratoDigital.Models
         public string TelefonoLabora { get; set; } //, (TEXTO) Solo números
         public string CelularOficina { get; set; } //, (TEXTO) Solo números
         public int DepartamentoLaboraId { get; set; } //, EXCEL ANEXO
-        public int CiudadLaboraId { get; set; } //, EXCEL ANEXO
+        public string CiudadLaboraId { get; set; } //, EXCEL ANEXO
         public int IngresoMensual { get; set; } //, (TEXTO) Solo números
         public int EgresoMensual { get; set; } //, (TEXTO) Solo números
         public string Profesion { get; set; } //, (TEXTO)
