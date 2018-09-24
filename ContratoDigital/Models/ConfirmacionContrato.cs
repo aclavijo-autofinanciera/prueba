7fd71c8c39222fcf904fb5df1d5537839f21bcd3
Luis Robles <dracvs@gmail.com>
2018-09-24 10:29:35 -0500

    Actualización de clase de contrato con valores que indican si fue instanciado en siicon, que día fue registrado y su estado

--- ContratoDigital/Models/ConfirmacionContrato.cs
+++ ContratoDigital/Models/ConfirmacionContrato.cs
@@ -17,10 +17,12 @@ namespace ContratoDigital.Models
         public bool IsAccepted { get; set; }
         public bool IsPaid { get; set; }
         public bool IsVerified { get; set; }
+        public bool IsRegistered { get; set; }
         public DateTime FechaVerificacion { get; set; }
         public DateTime FechaAceptacion { get; set; }
         public DateTime FechaPago { get; set; }
         public DateTime FechaReferenciaPago { get; set; }
+        public DateTime FechaRegistro { get; set; }
 
         public int TipoMedio { get; set; }
         public string DescripcionTipoMedio { get; set; }
@@ -33,6 +35,12 @@ namespace ContratoDigital.Models
         public int Asesor { get; set; }
         public string UserId { get; set; }
         public int IdContratoSiicon { get; set; }
+
+        // ID de registro de personas en el Siicon
+        public int IdSuscriptor { get; set; }
+        public int IdSuscriptorConjunto { get; set; }
+        public int IdRepresentanteLegal { get; set; }
+        public int IdRepresentanteLegalConjunto { get; set; }
 
         public virtual Contrato Contrato { get; set; }
     }
