26c6ca5294e2408e118b05044468453ad1bdbd93
Luis Robles <dracvs@gmail.com>
2018-11-21 11:37:50 -0500

    Modelo para las clases de Recibo de pago y pagos efectuados

---
+++ ContratoDigital/Models/Pagos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContratoDigital.Models
+{
+    public class Pagos
+    {
+        [Key]
+        public int IdPago { get; set; }
+        public double Monto { get; set; }
+        public int IdContrato { get; set; }
+        public DateTime FechaPago { get; set; }
+        public string ReferenciaSiicon { get; set; }
+
+        public virtual Contrato Contrato { get; set; }
+    }
+}
---
+++ ContratoDigital/Models/RecibosPago.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContratoDigital.Models
+{
+    public class RecibosPago
+    {
+        [Key]
+        public int IdRecibo { get; set; }
+        public double Monto { get; set; }
+        public int IdContrato { get; set; }
+        public DateTime FechaEmision {get;set;}
+        public string ReferenciaSiicon { get; set; }
+
+        public virtual Contrato Contrato { get; set; }
+    }
+}
