eabb883fc732ef8884abd86df141139d89a190ae
Luis Robles <dracvs@gmail.com>
2019-01-16 11:14:14 -0500

    Agregado controlador de oportunidades

---
+++ ContratoDigital/Controllers/OportunidadesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContratoDigital.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContratoDigital.Controllers
+{
+    public class OportunidadesController : Controller
+    {
+        public OportunidadesController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CargaMasiva()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CargaMasiva(PlainText _plaintext)
+        {
+            return  View();
+        }
+
+
+    }
+}
