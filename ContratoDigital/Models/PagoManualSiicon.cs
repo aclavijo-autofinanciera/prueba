26155ac60784d5a211c600a3fe8dceb6cb818f8a
Luis Robles <dracvs@gmail.com>
2019-02-21 17:32:17 -0500

    Clase modelo del pago manual para Siicon

---
+++ ContratoDigital/Models/PagoManualSiicon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContratoDigital.Models
+{
+    public class PagoManualSiicon
+    {
+        public int TipoPagoId { get; set; }
+        public DateTime FechaPago { get; set; }
+        public int CodCuentaBancaria { get; set; }
+        public int monto { get; set; }
+        public string Referencia { get; set; }
+        public string Numero { get; set; }
+        public int CodConcesionario { get; set; }
+        public int ContratoId { get; set; }
+        public int Contrato { get; set; }
+        public string TerceroGeneradorId { get; set; }
+        public string CompañiaId { get; set; }
+    }
+}
