1e6c6fd97cce5421228425c702ed0871089396fe
Luis Robles <lrobles@autofinanciera.com.co>
2019-03-19 15:51:14 -0500

    Contexto de conexión a la BD

---
+++ Freyja/Data/FreyjaContext.cs
@@ -0,0 +1,16 @@
+﻿using Freyja.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freyja.Data
+{
+    public class FreyjaContext :DbContext
+    {
+        public FreyjaContext(DbContextOptions<FreyjaContext> options) : base(options){ }
+        public DbSet<PagoTuCompra> PagoTuCompra { get; set; }
+        
+    }
+}
