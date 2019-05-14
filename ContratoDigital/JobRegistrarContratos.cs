4c6326a5a8b056b01b20d2f50366ba77bb74b6e2
Luis Robles <lrobles@autofinanciera.com.co>
2019-05-14 12:05:29 -0500

    Captura de excepciones para evitar que el hilo muera en caso de error catastrófico.

--- ContratoDigital/JobRegistrarContratos.cs
+++ ContratoDigital/JobRegistrarContratos.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace ContratoDigital
 {
-    internal class JobRegistrarContratos : IHostedService, IDisposable
+    internal class JobRegistrarContratos : IHostedService
     {
         private Timer _timer;
         private readonly ICanonicalUrlConfiguration _canonicalUrlConfiguration;
@@ -23,17 +23,33 @@ namespace ContratoDigital
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            _timer = new Timer(RegisterSiiconContracts, null, TimeSpan.Zero, TimeSpan.FromHours(6));
+            try
+            {
+                _timer = new Timer(RegisterSiiconContracts, null, TimeSpan.Zero, TimeSpan.FromHours(6));                
+                //throw new NotImplementedException();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[EXCEPTION STARTASYNC]: " + ex.Message);
+                Console.WriteLine("[STACKTRACE]: " + ex.StackTrace);
+            }
             return Task.CompletedTask;
-            //throw new NotImplementedException();
         }
 
         private void RegisterSiiconContracts(object state)
         {
-            using (var client = new HttpClient())
+            try
             {
-                CanonicalUrlService urlService = new CanonicalUrlService(_canonicalUrlConfiguration);                                
-                var result = client.GetStringAsync(urlService.GetCanonicalUrl() + "api/Freyja/RegistrarContratos").Result;                
+                using (var client = new HttpClient())
+                {
+                    CanonicalUrlService urlService = new CanonicalUrlService(_canonicalUrlConfiguration);
+                    var result = client.GetStringAsync(urlService.GetCanonicalUrl() + "api/Freyja/RegistrarContratos").Result;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("[EXCEPTION REGISTERCONTRACTS]: " + ex.Message);
+                Console.WriteLine("[STACKTRACE]: " + ex.StackTrace);
             }
         }
 
@@ -44,9 +60,10 @@ namespace ContratoDigital
             _timer?.Change(Timeout.Infinite, 0);
             return Task.CompletedTask;
         }
-        public void Dispose()
+
+        /*public void Dispose()
         {
             _timer?.Dispose();
-        }
+        }*/
     }
 }
