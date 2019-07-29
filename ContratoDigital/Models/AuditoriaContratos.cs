90678f1cf0a2df43c5f6621a69eb138fa215fa12
Luis Robles <dracvs@gmail.com>
2019-07-29 17:02:28 -0500

    Se añade las clases de auditoria

---
+++ ContratoDigital/Models/AuditoriaContratos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContratoDigital.Models
+{
+    public class AuditoriaContratos
+    {
+        [Key]
+        public int IdAuditoria { get; set; }
+        public int IdContrato { get; set; }
+        public string TipoDeMovimiento { get; set; }
+        public string DatosPrevios { get; set; }
+        public string DatosNuevos { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public string UsuarioRegistrante { get; set; }
+        public string IPRegistro { get; set; }
+    }
+}
